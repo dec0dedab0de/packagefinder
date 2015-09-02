a0e50d19e1147f31d9fd2227c2e0610e5df4ea6e
James P Robinson Jr <JamesPRobinsonJR@gmail.com>
2015-09-02 19:54:00 -0400

    added basic setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
+#may work with older versions of pip, should find out.
 setup(name='requirementsfinder',
       version='0.001',
       py_modules=['requirementsfinder'],
+      requires = ['pip>=6.1.1'],
+
       )
