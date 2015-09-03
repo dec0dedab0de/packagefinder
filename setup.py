b07e89689c65dd158a0c461f9e795ac3c10db4ba
James P Robinson Jr <JamesPRobinsonJR@gmail.com>
2015-09-02 23:11:10 -0400

    updated setup.. maybe this will make install work better

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup
 
-#may work with older versions of pip, should find out.
+# may work with older versions of pip, should find out.
 setup(name='requirementsfinder',
       version='0.001',
       py_modules=['requirementsfinder'],
-      requires = ['pip>=6.1.1'],
-
+      requires=['pip>=6.1.1'],
+      author='James P Robinson Jr',
+      author_email="JamesPRobinsonJr@gmail.com",
+      entry_points={
+          "console_scripts": [
+              "requirementsfinder=requirementsfinder:main",
+          ],
+      },
+      license='MIT',
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 2.7",
+    ],
       )
